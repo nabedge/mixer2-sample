af1a6a1fa1cd8adb20a50dc5a8cd672c77891726
nabedge <nabedge@gmail.com>
2012-12-02 21:32:11 +0900

    refix

--- mixer2-fruitshop-sastruts/src/main/java/org/mixer2/sample/action/M2staticAction.java
+++ mixer2-fruitshop-sastruts/src/main/java/org/mixer2/sample/action/M2staticAction.java
@@ -32,7 +32,7 @@ public class M2staticAction {
      * @return
      * @throws IOException
      */
-    @Execute(validator = false)
+    @Execute(validator = false, urlPattern = "m2static")
     public String outputImage() throws IOException {
 
         // create static file path
