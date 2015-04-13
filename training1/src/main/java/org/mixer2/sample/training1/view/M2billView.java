7eb27cc1613f7434a6f52e6b67b6877587b71f99
nabedge <yu.watanabe@bizreach.co.jp>
2015-04-13 19:34:53 +0900

    coding...

--- training1/src/main/java/org/mixer2/sample/training1/view/M2billView.java
+++ training1/src/main/java/org/mixer2/sample/training1/view/M2billView.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.BooleanUtils;
 import org.joda.time.DateTime;
 import org.mixer2.jaxb.xhtml.Html;
+import org.mixer2.jaxb.xhtml.Span;
 import org.mixer2.jaxb.xhtml.Tbody;
 import org.mixer2.sample.training1.bean.Bill;
 import org.mixer2.sample.training1.bean.Detail;
@@ -21,49 +22,49 @@ import org.mixer2.xhtml.builder.TableBuilder;
  */
 public class M2billView extends AbstractMixer2XhtmlView {
 
-    @Override
-    public Html renderHtml(Html html, Map<String, Object> model,
-            HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+	@Override
+	public Html renderHtml(Html html, Map<String, Object> model,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
 
-        // コントローラから渡された請求情報
-        Bill bill = (Bill) model.get("bill");
+		// コントローラから渡された請求情報
+		Bill bill = (Bill) model.get("bill");
 
-        // コントローラから再発行フラグも与えられている場合に再発行の表示を追加する.
-        Boolean reissue = (Boolean) model.get("reissue");
-        if (BooleanUtils.isTrue(reissue)) {
-            html.insertAfterId("billIssueDate", "(再発行)");
-        }
+		// コントローラから再発行フラグも与えられている場合に再発行の表示を追加する.
+		Boolean reissue = (Boolean) model.get("reissue");
+		if (BooleanUtils.isTrue(reissue)) {
+			html.insertAfterId("billIssueDate", "(再発行)");
+		}
 
-        // 請求書の内容を埋め込む
-        html.replaceById("billDestination", bill.getDestination());
-        html.replaceById("billTitle", bill.getTitle());
-        html.replaceById("billIssueDate",
-                new DateTime(bill.getIssueDate()).toString("yyyy-MM-dd"));
-        // 請求の詳細の部分はMixer2のTableビルダーを使ってtr,tdタグを作り、
-        // テンプレートのtbodyの中をごっそり入れ替えるためのtr/tdタグを新たに生成する
-        TableBuilder tb = new TableBuilder();
-        for (Detail d : bill.getDetailList()) {
-            tb.addTr()
-                .addTd(d.getProductName())
-                .addTd(Integer.toString(d.getCount()))
-                .addTd(d.getUnitPrice().toPlainString())
-                .addTd(d.getSubtotal().toPlainString());
-        }
-        //tbodyタグのなかをカラにする
-        html.getById("billDetailList", Tbody.class).unsetTr(); 
-        // tbodyタグの中に作ったtrタグを入れる
-        html.getById("billDetailList", Tbody.class).getTr().addAll(tb.build().getTr());
-        // 合計金額
-        html.replaceById("billCharge", bill.getCharge().toPlainString());
+		// 請求書の内容を埋め込む
+		html.getById("billDestination", Span.class).replaceInner(
+				bill.getDestination());
+		html.getById("billTitle", Span.class).replaceInner(bill.getTitle());
+		html.getById("billIssueDate", Span.class).replaceInner(
+				new DateTime(bill.getIssueDate()).toString("yyyy-MM-dd"));
+		// 請求の詳細の部分はMixer2のTableビルダーを使ってtr,tdタグを作り、
+		// テンプレートのtbodyの中をごっそり入れ替えるためのtr/tdタグを新たに生成する
+		TableBuilder tb = new TableBuilder();
+		for (Detail d : bill.getDetailList()) {
+			tb.addTr().addTd(d.getProductName())
+					.addTd(Integer.toString(d.getCount()))
+					.addTd(d.getUnitPrice().toPlainString())
+					.addTd(d.getSubtotal().toPlainString());
+		}
+		// tbodyタグのなかをカラにする
+		html.getById("billDetailList", Tbody.class).unsetTr();
+		// tbodyタグの中に作ったtrタグを入れる
+		html.getById("billDetailList", Tbody.class).getTr()
+				.addAll(tb.build().getTr());
+		// 合計金額
+		html.getById("billCharge", Span.class).replaceInner(bill.getCharge().toPlainString());
 
-        // すべての静的ファイルへの相対パスを絶対urlに変換する
-        Pattern pattern = Pattern.compile("^\\.+/static/(.*)$");
-        String ctx = request.getContextPath();
-        PathAdjuster.replacePath(html, pattern, "http://localhost:8080" + ctx
-                + "/$1");
+		// すべての静的ファイルへの相対パスを絶対urlに変換する
+		Pattern pattern = Pattern.compile("^\\.+/static/(.*)$");
+		String ctx = request.getContextPath();
+		PathAdjuster.replacePath(html, pattern, "http://localhost:8080" + ctx
+				+ "/$1");
 
-        return html;
-    }
-
+		return html;
+	}
 }
