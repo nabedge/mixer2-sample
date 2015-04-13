7e2f27e9b0cf5329a64e4acf360e91df017ce658
nabedge <yu.watanabe@bizreach.co.jp>
2015-04-13 20:23:19 +0900

    coding...

--- training1/src/main/java/org/mixer2/sample/training1/controller/BillController.java
+++ training1/src/main/java/org/mixer2/sample/training1/controller/BillController.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.IOUtils;
 import org.mixer2.Mixer2Engine;
 import org.mixer2.jaxb.xhtml.Html;
 import org.mixer2.sample.training1.bean.Bill;
+import org.mixer2.sample.training1.form.BillForm;
 import org.mixer2.sample.training1.service.BillService;
 import org.mixer2.sample.training1.view.M2billView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,6 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
@@ -24,78 +24,70 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 @RequestMapping("/bill")
 public class BillController {
 
-    @Autowired
-    protected Mixer2Engine mixer2Engine;
+	@Autowired
+	protected Mixer2Engine mixer2Engine;
 
-    @Autowired
-    protected ResourceLoader resourceLoader;
+	@Autowired
+	protected ResourceLoader resourceLoader;
 
-    @Autowired
-    protected BillService billService;
+	@Autowired
+	protected BillService billService;
 
-    @RequestMapping("/json")
-    @ResponseBody
-    public Bill json() {
-        Bill bill = billService.createBill();
-        return bill;
-    }
+	@RequestMapping("/json")
+	@ResponseBody
+	public Bill json() {
+		Bill bill = billService.createBill();
+		return bill;
+	}
 
-    @RequestMapping("/html")
-    public String html(
-            Model model,
-            @RequestParam(required = false, defaultValue = "false") boolean reissue) {
-        Bill bill = json();
-        model.addAttribute("bill", bill);
-        // 再発行の表示を入れるかのフラグもmodelに入れておく（あとでviewに渡す）
-        model.addAttribute("reissue", reissue);
-        return "bill";
-    }
+	@RequestMapping("/html")
+	public String html(Model model, BillForm billForm) {
+		Bill bill = json();
+		model.addAttribute("bill", bill);
+		// 再発行の表示を入れるかのフラグもmodelに入れておく（あとでviewに渡す）
+		model.addAttribute("reissue", billForm.isReissue());
+		return "bill";
+	}
 
-    @RequestMapping("/m2html")
-    public String m2html(
-            Model model,
-            @RequestParam(required = false, defaultValue = "false") boolean reissue) {
-        Bill bill = json();
-        model.addAttribute("bill", bill);
-        // 再発行の表示を入れるかのフラグもmodelに入れておく（あとでviewに渡す）
-        model.addAttribute("reissue", reissue);
-        return "m2bill";
-    }
+	@RequestMapping("/m2html")
+	public String m2html(Model model, BillForm billForm) {
+		Bill bill = json();
+		model.addAttribute("bill", bill);
+		// 再発行の表示を入れるかのフラグもmodelに入れておく（あとでviewに渡す）
+		model.addAttribute("reissue", billForm.isReissue());
+		return "m2bill";
+	}
 
-    @RequestMapping(value = "/pdf", produces = "application/pdf")
-    public void pdf(
-            Model model,
-            @RequestParam(required = false, defaultValue = "false") boolean reissue,
-            HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+	@RequestMapping(value = "/pdf", produces = "application/pdf")
+	public void pdf(Model model, BillForm billForm, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 
-        // pdfに埋め込む値は画面と同じように取得してmodelに入れる
-        Bill bill = json();
-        model.addAttribute("bill", bill);
+		// pdfに埋め込む値は画面と同じように取得してmodelに入れる
+		Bill bill = json();
+		model.addAttribute("bill", bill);
 
-        // 再発行の表示を入れるかのフラグもmodelに入れておく（あとでviewに渡す）
-        model.addAttribute("reissue", reissue);
+		// 再発行の表示を入れるかのフラグもmodelに入れておく（あとでviewに渡す）
+		model.addAttribute("reissue", billForm.isReissue());
 
-        // web画面用のテンプレートを読み込んでmixer2のhtml型のインスタンスを作る
-        InputStream is = resourceLoader.getResource(
-                "classpath:/templates/m2bill.html").getInputStream();
-        Html tmplHtml = mixer2Engine.loadHtmlTemplate(is);
-        IOUtils.closeQuietly(is);
+		// web画面用のテンプレートを読み込んでmixer2のhtml型のインスタンスを作る
+		InputStream is = resourceLoader.getResource(
+				"classpath:/templates/m2bill.html").getInputStream();
+		Html tmplHtml = mixer2Engine.loadHtmlTemplate(is);
+		IOUtils.closeQuietly(is);
 
-        // 読み込んだテンプレートに対し、html画面表示用のビュークラスを流用して値の埋め込みを行う
-        M2billView m2billView = new M2billView();
-        Html html = m2billView.renderHtml(tmplHtml, model.asMap(), request,
-                response);
+		// 読み込んだテンプレートに対し、html画面表示用のビュークラスを流用して値の埋め込みを行う
+		M2billView m2billView = new M2billView();
+		Html html = m2billView.renderHtml(tmplHtml, model.asMap(), request,
+				response);
 
-        // headerの部分はpdfでは不要なので削除
-        html.removeById("header");
+		// headerの部分はpdfでは不要なので削除
+		html.removeById("header");
 
-        // htmlをstringに戻し、さらにそこからpdfを生成してresponseに詰め込む
-        String htmlStr = mixer2Engine.saveToString(html);
-        ITextRenderer itr = new ITextRenderer();
-        itr.setDocumentFromString(htmlStr);
-        itr.layout();
-        itr.createPDF(response.getOutputStream());
-    }
-
+		// htmlをstringに戻し、さらにそこからpdfを生成してresponseに詰め込む
+		String htmlStr = mixer2Engine.saveToString(html);
+		ITextRenderer itr = new ITextRenderer();
+		itr.setDocumentFromString(htmlStr);
+		itr.layout();
+		itr.createPDF(response.getOutputStream());
+	}
 }
