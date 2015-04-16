e4597394a3b3b9c324edd6f28e933b384c568d1e
nabedge <nabedge@gmail.com>
2015-04-16 11:36:31 +0900

    tomcatのJarScannerのscanClassPathをfalseに上書きする設定を加えてみたサンプル

--- training1/src/main/java/org/mixer2/sample/training1/Application.java
+++ training1/src/main/java/org/mixer2/sample/training1/Application.java
@@ -1,13 +1,19 @@
 package org.mixer2.sample.training1;
 
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.Filter;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.mixer2.Mixer2Engine;
 import org.mixer2.spring.webmvc.Mixer2XhtmlViewResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
@@ -16,56 +22,76 @@ import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-    
-    @Bean
-    public Mixer2Engine mixer2Engine() {
-    	return new Mixer2Engine();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Bean
-    public VelocityConfigurer velocityConfigrer() {
-    	VelocityConfigurer vc = new VelocityConfigurer();
-    	vc.setResourceLoaderPath("classpath:/templates/");
-    	Properties props = new Properties();
-    	props.setProperty("input.encoding", "UTF-8");
-    	props.setProperty("output.encoding", "UTF-8");
-    	vc.setVelocityProperties(props);
-    	return vc;
-    }
-    
+	@Bean
+	public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
 
-    @Bean
-    public VelocityViewResolver velocityViewResolver() {
-    	VelocityViewResolver resolver = new VelocityViewResolver();
-    	resolver.setCache(false);
-    	resolver.setSuffix(".vm");
-    	resolver.setContentType("text/html; charset=UTF-8");
-    	resolver.setDateToolAttribute("dateTool");
-    	resolver.setNumberToolAttribute("numberTool");
-    	resolver.setOrder(101);
-    	return resolver;
-    }
-    
-    @Bean
-    public Mixer2XhtmlViewResolver mixer2XhtmlViewResolver() {
-        Mixer2XhtmlViewResolver resolver = new Mixer2XhtmlViewResolver();
-        resolver.setPrefix("classpath:/templates/");
-        resolver.setSuffix(".html");
-        resolver.setBasePackage("org.mixer2.sample.training1.view");
-        resolver.setMixer2Engine(mixer2Engine());
-        resolver.setOrder(100);
-        return resolver;
-    }
-    
-    @Bean
-    public Filter characterEncodingFilter() {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        return filter;
-    }
+		factory.setUriEncoding("UTF-8");
+		//factory.setContextPath("/hoge");
+		factory.setSessionTimeout(1800, TimeUnit.SECONDS);
+		factory.addContextCustomizers(new TomcatContextCustomizer() {
+			@Override
+			public void customize(Context context) {
+				StandardJarScanner jarScanner = new StandardJarScanner();
+				jarScanner.setScanAllFiles(false);
+				jarScanner.setScanBootstrapClassPath(false);
+				jarScanner.setScanAllDirectories(false);
+				jarScanner.setScanClassPath(false);
+				context.setJarScanner(jarScanner);
+			}
+		});
+		return factory;
+	}
+
+	@Bean
+	public Mixer2Engine mixer2Engine() {
+		return new Mixer2Engine();
+	}
+
+	@Bean
+	public VelocityConfigurer velocityConfigrer() {
+		VelocityConfigurer vc = new VelocityConfigurer();
+		vc.setResourceLoaderPath("classpath:/templates/");
+		Properties props = new Properties();
+		props.setProperty("input.encoding", "UTF-8");
+		props.setProperty("output.encoding", "UTF-8");
+		vc.setVelocityProperties(props);
+		return vc;
+	}
+
+	@Bean
+	public VelocityViewResolver velocityViewResolver() {
+		VelocityViewResolver resolver = new VelocityViewResolver();
+		resolver.setCache(false);
+		resolver.setSuffix(".vm");
+		resolver.setContentType("text/html; charset=UTF-8");
+		resolver.setDateToolAttribute("dateTool");
+		resolver.setNumberToolAttribute("numberTool");
+		resolver.setOrder(101);
+		return resolver;
+	}
+
+	@Bean
+	public Mixer2XhtmlViewResolver mixer2XhtmlViewResolver() {
+		Mixer2XhtmlViewResolver resolver = new Mixer2XhtmlViewResolver();
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setBasePackage("org.mixer2.sample.training1.view");
+		resolver.setMixer2Engine(mixer2Engine());
+		resolver.setOrder(100);
+		return resolver;
+	}
+
+	@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		return filter;
+	}
 
 }
