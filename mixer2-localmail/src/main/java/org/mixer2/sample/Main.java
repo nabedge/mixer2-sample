f287af9029707852a01c3d748b4cb869921dd606
MizukiNakano <mizuki590@gmail.com>
2015-07-12 13:23:30 +0900

    Add Main.java

---
+++ mixer2-localmail/src/main/java/org/mixer2/sample/Main.java
@@ -0,0 +1,62 @@
+package org.mixer2.sample;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.mixer2.Mixer2Engine;
+import org.mixer2.jaxb.xhtml.H1;
+import org.mixer2.jaxb.xhtml.Html;
+import org.mixer2.jaxb.xhtml.P;
+import org.mixer2.xhtml.exception.TagTypeUnmatchException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.mail.*;
+
+public class Main {
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	// Server setting
+	private static final String HOST_NAME = "localhost";
+	private static final int PORT = 8080;
+
+	//
+	private static final String TO_ADDR = "nabedge@mixer2.org", TO_NAME = "nabedge",
+			FROM_ADDR = "me@mixer2.org", FROM_NAME = "me",
+			SUBJECT = "Sample-title";
+
+	public static void main(String[] args) throws EmailException {
+		// Create the email message
+		HtmlEmail email = new HtmlEmail();
+		email.setHostName(HOST_NAME);
+		email.setSmtpPort(PORT);
+		email.addTo(TO_ADDR, TO_NAME);
+		email.setFrom(FROM_ADDR, FROM_NAME);
+		email.setSubject(SUBJECT);
+
+		// Create Html from template
+		InputStream is = Main.class.getClassLoader().getResourceAsStream("m2mockup/m2template/mail.html");
+		Mixer2Engine m2e = new Mixer2Engine();
+		Html html;
+		try {
+			// Set the html message
+			html = m2e.loadHtmlTemplate(is);
+			html.getById("headline", H1.class).replaceInner("This is sample to apply mixer2 to E-mail");
+			html.getById("content", P.class).replaceInner("Success to send E-mail");
+			email.setHtmlMsg(m2e.saveToString(html));
+		} catch (IOException e) {
+			logger.warn("Mixer2 failed to loadHTMLTemplate");
+			e.printStackTrace();
+		} catch (TagTypeUnmatchException e) {
+			logger.warn("Mixer2 failed to getById");
+			e.printStackTrace();
+		}
+
+		// Set the alternative message
+		email.setTextMsg("Your email client does not support HTML messages");
+
+		// send the email
+		email.send();
+		logger.debug("Send E-mail");
+	}
+
+}
